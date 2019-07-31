e4350afa885dc441c34ed3d34efd4c681d539e15
akhlaq1 <akhlaq.computer@gmail.com>
2019-07-31 15:53:04 +0500

    v1

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path=require('path')
+
+
+app.use(express.static(__dirname+'/node_modules'));
+
+app.use(express.static(__dirname+'/css'));
+app.use(express.static(__dirname+'/font-awesome'));
+app.use(express.static(__dirname+'/fonts'));
+app.use(express.static(__dirname+'/img'));
+app.use(express.static(__dirname+'/js'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+console.log("Express server working")
