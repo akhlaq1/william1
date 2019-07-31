0c52c306773b8158615ef07441712815abaf00c6
akhlaq1 <akhlaq.computer@gmail.com>
2019-07-31 17:05:24 +0500

    v4

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const app = express();
 const path=require('path')
 
 
-app.use(express.static(__dirname+'/node_modules'));
 
-app.use(express.static(__dirname+'/js'));
-app.use(express.static(__dirname+'/css'));
-app.use(express.static(__dirname+'/font-awesome'));
-app.use(express.static(__dirname+'/fonts'));
-app.use(express.static(__dirname+'/img'));
+
+
+    app.use(express.static(__dirname+'/node_modules'));
+
+    app.use("/css",express.static(__dirname+'/css'));
+    app.use('/font-awesome',express.static(__dirname+'/font-awesome'));
+    app.use('/fonts',express.static(__dirname+'/fonts'));
+    app.use('/img',express.static(__dirname+'/img'));
+    app.use('/js',express.static(__dirname+'/js'));
 
 
 app.listen(port=8080);
